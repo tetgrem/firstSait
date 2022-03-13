8c1777b567db7c2a8548bc2769e63a0cbaa349ec
tetgrem <tetgrem@gmail.com>
2022-03-13 20:32:39 +0200

    done

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,99 @@
 // Custom scripts
+
+// Модальное окно
+function bindModal(trigger, modal, close) {
+    trigger = document.querySelector(trigger),
+        modal = document.querySelector(modal),
+        close = document.querySelector(close)
+
+    const body = document.body
+
+    trigger.addEventListener('click', e => {
+        e.preventDefault()
+        modal.style.display = 'flex'
+        body.classList.add('locked')
+    });
+    close.addEventListener('click', () => {
+        modal.style.display = 'none'
+        body.classList.remove('locked')
+    });
+    modal.addEventListener('click', e => {
+        if (e.target === modal) {
+            modal.style.display = 'none'
+            body.classList.remove('locked')
+        }
+    })
+}
+
+// ПЕРВЫЙ аргумент - класс кнопки, при клике на которую будет открываться модальное окно.
+// ВТОРОЙ аргумент - класс самого модального окна.
+// ТРЕТИЙ аргумент - класс кнопки, при клике на которую будет закрываться модальное окно.
+bindModal('.tellbutton', '.modal__wrapper', '.modal__close')
+bindModal('.tellbutton1', '.modal__wrapper', '.modal__close')
+
+
+
+function burgerMenu() {
+    const burger = document.querySelector('.burger')
+    const menu = document.querySelector('.menu')
+    const body = document.querySelector('body')
+    burger.addEventListener('click', () => {
+        if (!menu.classList.contains('active')) {
+            menu.classList.add('active')
+            burger.classList.add('active-burger')
+            body.classList.add('locked')
+        } else {
+            menu.classList.remove('active')
+            burger.classList.remove('active-burger')
+            body.classList.remove('locked')
+        }
+    })
+    // Вот тут мы ставим брейкпоинт навбара
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 991.98) {
+            menu.classList.remove('active')
+            burger.classList.remove('active-burger')
+            body.classList.remove('locked')
+        }
+    })
+}
+burgerMenu()
+
+
+window.addEventListener("DOMContentLoaded", function() {
+    [].forEach.call( document.querySelectorAll('.tel'), function(input) {
+        var keyCode;
+        function mask(event) {
+            event.keyCode && (keyCode = event.keyCode);
+            var pos = this.selectionStart;
+            if (pos < 3) event.preventDefault();
+            var matrix = "+7 (___) ___ ____",
+                i = 0,
+                def = matrix.replace(/\D/g, ""),
+                val = this.value.replace(/\D/g, ""),
+                new_value = matrix.replace(/[_\d]/g, function(a) {
+                    return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+                });
+            i = new_value.indexOf("_");
+            if (i != -1) {
+                i < 5 && (i = 3);
+                new_value = new_value.slice(0, i)
+            }
+            var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+                function(a) {
+                    return "\\d{1," + a.length + "}"
+                }).replace(/[+()]/g, "\\$&");
+            reg = new RegExp("^" + reg + "$");
+            if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+            if (event.type == "blur" && this.value.length < 5)  this.value = ""
+        }
+
+        input.addEventListener("input", mask, false);
+        input.addEventListener("focus", mask, false);
+        input.addEventListener("blur", mask, false);
+        input.addEventListener("keydown", mask, false)
+
+    });
+
+});
+
